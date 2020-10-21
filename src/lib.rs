0baef32754bf7b8a0f15a114cd87a6aad4e3db6a
Costin Ionescu <costin.ionescu@gmail.com>
2020-10-21 07:35:26 -0700

    split lib

---
+++ src/lib.rs
@@ -0,0 +1,18 @@
+#![no_std]
+
+pub fn lib_name() -> &'static str {
+    "halfbit"
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn lib_name_compliant() {
+        let n: &'static str = lib_name();
+        assert!(n.contains("halfbit"));
+    }
+
+}
+
