81ba280236ebed60bda213c894c7053c7a8dc52e
Costin Ionescu <costin.ionescu@gmail.com>
2020-12-10 20:39:14 -0800

    tarpaulin coverage 100% on bump_alloc

--- src/mm/bump_alloc.rs
+++ src/mm/bump_alloc.rs
@@ -51,8 +51,8 @@ unsafe impl<'a> Allocator for BumpAllocator<'a> {
         size: NonZeroUsize,
         align: Pow2Usize
     ) -> Result<NonNull<u8>, AllocError> {
-        let state: &'a mut BumpAllocatorState<'a> =
-            &mut *(self.state.get() as *mut BumpAllocatorState<'a>);
+        let state: &'a mut BumpAllocatorState<'a> = &mut
+            *(self.state.get() as *mut BumpAllocatorState<'a>);
         usize_align_up(state.current_addr, align)
             .map_or(None, |v| v.checked_add(size.get()))
             .map_or(None, |v| if v <= state.end_addr {
@@ -69,9 +69,8 @@ unsafe impl<'a> Allocator for BumpAllocator<'a> {
         _align: Pow2Usize
     ) {
         if self.is_last_allocation(ptr, current_size) {
-            let state: &'a mut BumpAllocatorState<'a> = {
-                &mut *(self.state.get() as *mut BumpAllocatorState<'a>)
-            };
+            let state: &'a mut BumpAllocatorState<'a> = &mut
+                *(self.state.get() as *mut BumpAllocatorState<'a>);
             state.current_addr -= current_size.get();
         }
     }
@@ -84,9 +83,8 @@ unsafe impl<'a> Allocator for BumpAllocator<'a> {
     ) -> Result<NonNull<u8>, AllocError> {
         if self.is_last_allocation(ptr, current_size) &&
             align.is_non_null_ptr_aligned(ptr) {
-            let state: &'a mut BumpAllocatorState<'a> = {
-                &mut *(self.state.get() as *mut BumpAllocatorState<'a>)
-            };
+            let state: &'a mut BumpAllocatorState<'a> = &mut 
+                *(self.state.get() as *mut BumpAllocatorState<'a>);
             let extra_size = new_larger_size.get() - current_size.get();
             if extra_size <= state.end_addr - state.current_addr {
                 state.current_addr += extra_size;
@@ -111,30 +109,22 @@ unsafe impl<'a> Allocator for BumpAllocator<'a> {
             Err(AllocError::UnsupportedAlignment)
         } else {
             if self.is_last_allocation(ptr, current_size) {
-                let state: &'a mut BumpAllocatorState<'a> = {
-                    &mut *(self.state.get() as *mut BumpAllocatorState<'a>)
-                };
+                let state: &'a mut BumpAllocatorState<'a> = &mut
+                    *(self.state.get() as *mut BumpAllocatorState<'a>);
                 state.current_addr -= current_size.get() - new_smaller_size.get();
             }
             Ok(ptr)
         }
     }
-    fn supports_contains(&self) -> bool {
-        true
-    }
-    fn contains(
-        &self,
-        ptr: NonNull<u8>
-    ) -> bool {
+    fn supports_contains(&self) -> bool { true }
+    fn contains(&self, ptr: NonNull<u8>) -> bool {
         let state: &'a BumpAllocatorState<'a> = unsafe {
             &*(self.state.get() as *mut BumpAllocatorState<'a>)
         };
         let addr = ptr.as_ptr() as usize;
         state.begin_addr <= addr && addr < state.end_addr
     }
-    fn name(&self) -> &'static str {
-        "bump-allocator"
-    }
+    fn name(&self) -> &'static str { "bump-allocator" }
 }
 
 #[cfg(test)]
@@ -339,4 +329,43 @@ mod tests {
         assert_eq!(unsafe { *p3.as_ptr() }, 0x5A_u8);
         assert_eq!(unsafe { *p2.as_ptr() }, 0xBB_u8);
     }
+
+    #[test]
+    fn shrink_with_higher_alignment_fails() {
+        let mut buffer = [0xAA_u8; 4];
+        let a = BumpAllocator::new(&mut buffer);
+        let p1 = unsafe { a.alloc(
+            NonZeroUsize::new(2).unwrap(),
+            Pow2Usize::one()
+        ) }.unwrap();
+        let e2 = unsafe { 
+            a.shrink(
+                p1,
+                NonZeroUsize::new(2).unwrap(),
+                NonZeroUsize::new(1).unwrap(),
+                Pow2Usize::max()
+            )
+        }.unwrap_err();
+        assert_eq!(e2, AllocError::UnsupportedAlignment);
+    }
+
+    #[test]
+    fn contains_is_supported() {
+        let mut buffer = [0xAA_u8; 4];
+        let a = BumpAllocator::new(&mut buffer);
+        assert!(a.supports_contains());
+    }
+
+    #[test]
+    fn contains_true_only_for_pointers_inside_buffer() {
+        let mut buffer = [0xAA_u8; 47];
+        let b = buffer.as_mut_ptr();
+        let n = buffer.len();
+        let a = BumpAllocator::new(&mut buffer);
+        assert!(a.contains(NonNull::new(b).unwrap()));
+        assert!(a.contains(NonNull::new(unsafe { b.offset(n as isize - 1) }).unwrap()));
+        assert!(!a.contains(NonNull::new(unsafe { b.offset(n as isize) }).unwrap()));
+        assert!(!a.contains(NonNull::new(unsafe { b.offset(-1) }).unwrap()));
+    }
+
 }
