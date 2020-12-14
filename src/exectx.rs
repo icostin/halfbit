24eea6a3c444d84b7255a302b78d5025bbad1773
Costin Ionescu <costin.ionescu@gmail.com>
2020-12-14 07:29:36 -0800

    xc.to_box()

--- src/exectx.rs
+++ src/exectx.rs
@@ -1,8 +1,10 @@
 //use crate::mm::Allocator;
 use crate::mm::AllocatorRef;
-//use crate::mm::AllocError;
+use crate::mm::Box;
+use crate::mm::AllocError;
 use crate::io::stream::Stream;
 
+/* ExecutionContext *********************************************************/
 pub struct ExecutionContext<'a> {
     main_allocator: AllocatorRef<'a>,
     error_allocator: AllocatorRef<'a>,
@@ -11,12 +13,32 @@ pub struct ExecutionContext<'a> {
 }
 
 impl<'a> ExecutionContext<'a> {
+
     pub fn new(
         main_allocator: AllocatorRef<'a>,
         error_allocator: AllocatorRef<'a>,
         log_stream: &'a mut (dyn Stream + 'a),
     ) -> ExecutionContext<'a> {
         ExecutionContext { main_allocator, error_allocator, log_stream }
+    }
+
+    pub fn get_main_allocator(&self) -> AllocatorRef<'_> {
+        self.main_allocator
+    }
+
+    pub fn get_error_allocator(&self) -> AllocatorRef<'_> {
+        self.error_allocator
+    }
+
+    pub fn get_log_stream(&mut self) -> &mut (dyn Stream + '_) {
+        self.log_stream
+    }
+
+    pub fn to_box<T: Sized>(
+        &self,
+        v: T
+    ) -> Result<Box<'_, T>, (AllocError, T)> {
+        self.get_main_allocator().alloc_item(v)
     }
 }
 
@@ -32,9 +54,32 @@ mod tests {
         let mut buf = [0_u8; 0x100];
         let a = BumpAllocator::new(&mut buf);
         let mut log = NullStream::new();
-        let xc = ExecutionContext::new(a.to_ref(), a.to_ref(), &mut log);
-        assert!(xc.main_allocator.name().contains("bump"));
-        assert!(xc.error_allocator.name().contains("bump"));
-        assert!(xc.log_stream.provider_name().contains("null-stream"));
+        let mut xc = ExecutionContext::new(a.to_ref(), a.to_ref(), &mut log);
+        assert!(xc.get_main_allocator().name().contains("bump"));
+        assert!(xc.get_error_allocator().name().contains("bump"));
+        assert!(xc.get_log_stream().provider_name().contains("null-stream"));
     }
+
+    #[test]
+    fn to_box_happy_case() {
+        let mut buf = [0_u8; 0x100];
+        let a = BumpAllocator::new(&mut buf);
+        let mut log = NullStream::new();
+        let xc = ExecutionContext::new(a.to_ref(), a.to_ref(), &mut log);
+        let b = xc.to_box(0x12345_u32).unwrap();
+        assert_eq!(*b, 0x12345_u32);
+    }
+
+    #[test]
+    fn to_box_fails() {
+        let mut buf = [0_u8; 3];
+        let a = BumpAllocator::new(&mut buf);
+        let mut log = NullStream::new();
+        let xc = ExecutionContext::new(a.to_ref(), a.to_ref(), &mut log);
+        let (e, v) = xc.to_box(0x12345_u32).unwrap_err();
+        assert_eq!(e, AllocError::NotEnoughMemory);
+        assert_eq!(v, 0x12345_u32);
+    }
+
+
 }
