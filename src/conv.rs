7ca731d379f3d7601d831993c19a862094ce92f1
Costin Ionescu <costin.ionescu@gmail.com>
2021-01-11 07:39:18 -0800

    uint_be conversion

--- src/conv.rs
+++ src/conv.rs
@@ -16,6 +16,19 @@ where T: num::traits::Unsigned + num::traits::int::PrimInt + core::ops::Shl + co
     }
 }
 
+pub fn uint_be_decode<T>(src: &[u8]) -> Option<T>
+where T: num::traits::Unsigned + num::traits::int::PrimInt + core::ops::Shl + core::ops::BitOr {
+    if src.len() < size_of::<T>() {
+        None
+    } else {
+        let mut v: T = num::zero();
+        for b in src[..size_of::<T>()].iter() {
+            v = (v << 8) | T::from(*b).unwrap();
+        }
+        Some(v)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -29,4 +42,16 @@ mod tests {
     fn u16le_decode() {
         assert_eq!(uint_le_decode::<u16>(b"\x12\x34").unwrap(), 0x3412);
     }
+
+    #[test]
+    fn u16be_on_truncated_buffer() {
+        assert_eq!(uint_be_decode::<u16>(b"\x12"), None);
+    }
+
+
+    #[test]
+    fn u16be_decode() {
+        assert_eq!(uint_be_decode::<u16>(b"\x12\x34").unwrap(), 0x1234);
+    }
+
 }
